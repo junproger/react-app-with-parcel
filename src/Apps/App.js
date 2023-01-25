27d087a36540aad7d61c2ae95204fb83774789d8
Junior Programmer <progphyton@gmail.com>
2023-01-25 07:51:42 +0300

    refactor: content and styles

--- src/Apps/App.js
+++ src/Apps/App.js
@@ -1,3 +1,12 @@
 export const App = () => {
-  return <h1>Hello, world!</h1>;
+  return (
+  <>
+    <h1 className="appTitle">
+      Hello, World!
+    </h1>
+    <p>
+      This pure React.js + Parcel.js bundler.
+    </p>
+  </>
+  );
 };
